cb024736e15a174a740a2abbf02554d1903a880f
buihai226 <buixuanhai2k4tb@gmail.com>
2025-05-16 19:16:52 +0700

    update main

--- main.py
+++ main.py
@@ -40,3 +40,9 @@ async def extract_captcha(file: UploadFile = File(...)):
         return captchas[0]
     else:
         return "Can't found"
+    
+    
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
+
